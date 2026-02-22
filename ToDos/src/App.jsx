326346d94a0bba8910685bd25964deec524b7174
Turjoy <saifbus28@gmail.com>
2026-02-22 20:46:07 +0600

    initial

---
+++ ToDos/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from "./components/HomeCards"
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
+import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './Pages/JobsPage';
+const router=createBrowserRouter(
+ 
+    createRoutesFromElements(
+     <Route path='/' element={<MainLayout/>}>
+    
+    <Route index element={<HomePage/>}/>
+    <Route path='/jobs' element={<JobsPage/>}/>
+  </Route>
+  
+)
+)
+
+const App = () => {
+  return <RouterProvider router={router}/>
+}
+
+export default App;
